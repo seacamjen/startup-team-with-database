ee21649f9208a72f01e758b9ac8d5e660b68ddc8
Cameron Jensen <jensenseanc@gmail.com>
2017-03-24 11:55:57 -0700

    finish able to show each team and member within that team.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,54 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("teams", Team.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/team-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String title = request.queryParams("title");
+      Team newTeam = new Team(title);
+      model.put("template", "templates/team-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Team team = Team.find(Integer.parseInt(request.params(":id")));
+      model.put("title", team);
+      model.put("template", "templates/team.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams/:id/members/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Team team = Team.find(Integer.parseInt(request.params(":id")));
+      model.put("title", team);
+      model.put("template", "templates/team-member-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/members", (request, reponse) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      Team team = Team.find(Integer.parseInt(request.queryParams("teamId")));
+
+      String name = request.queryParams("name");
+      Member newMember = new Member(name);
+
+      team.addMember(newMember);
+
+      model.put("title", team);
+      model.put("template", "templates/team-member-success.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+  }
+}
