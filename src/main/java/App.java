3184fe7024e5798e3799187f2708771565214b0d
Cameron Jensen <jensenseanc@gmail.com>
2017-03-24 09:41:20 -0700

    add first route on app.java page.

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
